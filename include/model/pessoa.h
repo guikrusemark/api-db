428bcd5e9d331cde4371a55f114334006344c0c9
guikrusemark <guilhermekrusemark@gmail.com>
2022-10-07 17:01:37 -0300

    added models .h

---
+++ include/model/pessoa.h
@@ -0,0 +1,17 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <string>
+#include <vector>
+
+class Pessoa {
+    private:
+        std::string m_nome;
+        std::string m_cpf;
+        std::vector<Endereco> m_enderecos;
+
+    public:
+
+};
+
+#endif // PESSOA_H
